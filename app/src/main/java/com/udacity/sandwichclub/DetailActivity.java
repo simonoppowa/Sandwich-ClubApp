729e9053c68b0106d577961cce7e626606566ab0
simonoppowa <simon.oppowa@st.oth-regensburg.de>
2018-03-06 13:58:14 +0100

    added DetailActivity/DetailXML

--- app/src/main/java/com/udacity/sandwichclub/DetailActivity.java
+++ app/src/main/java/com/udacity/sandwichclub/DetailActivity.java
@@ -3,7 +3,9 @@ package com.udacity.sandwichclub;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 import android.widget.ImageView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.squareup.picasso.Picasso;
@@ -12,15 +14,31 @@ import com.udacity.sandwichclub.utils.JsonUtils;
 
 import org.json.JSONException;
 
+import java.util.List;
+
 public class DetailActivity extends AppCompatActivity {
 
     public static final String EXTRA_POSITION = "extra_position";
     private static final int DEFAULT_POSITION = -1;
 
+    private TextView placeOfOriginTV;
+    private TextView placeOfOriginLabelTV;
+    private TextView descriptionTV;
+    private TextView ingredientsTV;
+    private TextView alsoKnownAsTV;
+    private TextView alsoKnownAsLabelTV;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detail);
+
+        placeOfOriginTV = findViewById(R.id.origin_tv);
+        placeOfOriginLabelTV = findViewById(R.id.placeOfOriginLabel);
+        descriptionTV = findViewById(R.id.description_tv);
+        ingredientsTV = findViewById(R.id.ingredients_tv);
+        alsoKnownAsTV = findViewById(R.id.also_known_tv);
+        alsoKnownAsLabelTV = findViewById(R.id.alsoKnowAsLabel);
 
         ImageView ingredientsIv = findViewById(R.id.image_iv);
 
@@ -50,7 +68,7 @@ public class DetailActivity extends AppCompatActivity {
             return;
         }
 
-        populateUI();
+        populateUI(sandwich);
         Picasso.with(this)
                 .load(sandwich.getImage())
                 .into(ingredientsIv);
@@ -63,7 +81,42 @@ public class DetailActivity extends AppCompatActivity {
         Toast.makeText(this, R.string.detail_error_message, Toast.LENGTH_SHORT).show();
     }
 
-    private void populateUI() {
+    private void populateUI(Sandwich sandwich) {
 
+        //origin TextView
+        if(sandwich.getPlaceOfOrigin().isEmpty()) {
+            hideTextView(placeOfOriginTV);
+            hideTextView(placeOfOriginLabelTV);
+        }
+        else {
+            placeOfOriginTV.setText(sandwich.getPlaceOfOrigin());
+        }
+
+        //alsoKnownAs TextView
+        if(sandwich.getAlsoKnownAs().isEmpty()) {
+            hideTextView(alsoKnownAsTV);
+            hideTextView(alsoKnownAsLabelTV);
+        }
+        else {
+            List<String> alsoKnownAs = sandwich.getAlsoKnownAs();
+
+            for(int i = 0; i < alsoKnownAs.size(); i++) {
+                alsoKnownAsTV.append(alsoKnownAs.get(i) + "\n");
+            }
+        }
+
+        //ingredients TextView
+        List<String> ingredients = sandwich.getIngredients();
+
+        for(int i = 0; i < ingredients.size(); i++) {
+            ingredientsTV.append(ingredients.get(i) + "\n");
+        }
+
+        //description TextView
+        descriptionTV.setText(sandwich.getDescription());
+    }
+
+    private void hideTextView(TextView textView) {
+        textView.setVisibility(View.INVISIBLE);
     }
 }
